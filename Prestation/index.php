6b9e0414950e8bffad857c31fd54b26499df0b0c
AzAchraf10 <azizicharaf10@gmail.com>
2025-04-12 00:23:32 +0100

    corriger erreur v1.7

--- Prestation/index.php
+++ Prestation/index.php
@@ -4,8 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    hello world!!!
+    <?php include '../nav.html'; ?>
+
+    <?php include '../footer.html'; ?>
 </body>
 </html>
